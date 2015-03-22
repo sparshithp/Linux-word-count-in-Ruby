3af7e4ea324022865f645dfa4c79d656b784d094
Sparshith P <sparsh.pro@gmail.com>
2015-03-21 22:01:04 -0700

    Added code to check if a line is beginnign of a new article and add it to a has if it is

--- parser.rb
+++ parser.rb
@@ -1,10 +1,16 @@
+def check_and_get_number(line)
+  if line.start_with? "Article" and line[-1, 1]=="."
+    words = line[0, line.length-1].split(/\s+/)
+    return words[1] if words.length==2
+  end
+  return -1
+end
+
 f = File.open(ARGV[0], "r")
 censor=["i", "we", "you", "they", "a", "and", "the", "that", "of", "for", "with"]
-lines=0
-characters=0
-prop_char=0
-word_count=0
-prop_words=0
+articles = Hash.new
+lines, characters, prop_char, word_count, prop_words =0, 0, 0, 0, 0
+curr_article, total_sections, sec_count = 0, 0, 0
 name = File.basename(f.path)
 f.each_line do |line|
   lines += 1
@@ -16,13 +22,18 @@ f.each_line do |line|
   word_count += words.length
   prop_words += words.length
   words.each do |w|
-      stripped_char = w.downcase.gsub(/\A[\d_\W]+|[\d_\W]+\Z/, '')
-      if censor.include? stripped_char
-        prop_char -= stripped_char.length
-        prop_words -= 1
-      end
+    stripped_char = w.downcase.gsub(/\A[\d_\W]+|[\d_\W]+\Z/, '')
+    prop_char -= stripped_char.length; prop_words -= 1 if censor.include? stripped_char
+  end
+  num = check_and_get_number(line)
+  next if num == -1
+  if num != curr_article
+    puts num
+    articles[num] = 0
   end
 end
-  puts "all:\t#{lines}\t#{word_count}\t#{characters}\t#{name}"
-  puts "proper:\t#{lines}\t#{prop_words}\t#{prop_char}\t#{name}"
+puts "all:\t#{lines}\t#{word_count}\t#{characters}\t#{name}"
+puts "proper:\t#{lines}\t#{prop_words}\t#{prop_char}\t#{name}"
+articles.each do |x,y| puts x end
 f.close
+
